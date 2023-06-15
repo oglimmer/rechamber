762d9f07ab394e4a247a2cad321d4d2c7204711b
Oli Zimpasser <oglimmer@gmail.com>
2023-06-16 00:15:06 +0200

    Add files previously ignored

---
+++ rechamber-api/src/config/Config.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\config;
+
+class Config
+{
+    public static string $DB_HOST = 'mysql_db';
+    public static string $DB_USERNAME = 'app_user';
+    public static string $DB_PASSWORD = 'app_pass';
+    public static string $DB_NAME = 'rechamberdb';
+    public static int $DB_PORT = 3306;
+}
